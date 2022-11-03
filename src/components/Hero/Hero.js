007ea06f1c1c790c14c24c2fe9fa1f6f2b30097a
AlphonseBrandon <alphonse.brand@gmail.com>
2022-11-03 21:07:07 +0100

    hero section added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello There! <br /> 
+        Glad to see you here.
+      </SectionTitle>
+      <SectionText>
+        I am a distinguished data scientist with a track record of building intelligent systems that solve real-world problems. I have a passion for data science and machine learning and I am always looking for opportunities to learn and grow.
+      </SectionText> 
+      <Button onClick={() => window.location = 'https://google.com'}>Hire Me</Button> 
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
