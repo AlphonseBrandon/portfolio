c107ce9a5a4791242614db5c4438e9461862ed2f
AlphonseBrandon <alphonse.brand@gmail.com>
2022-11-22 12:14:29 +0100

    Google analytics g4 added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,16 @@
 import Theme from '../styles/theme';
 // Google analytics
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
-const TrackingId = 'UA-249995739-2';
-ReactGA.initialize(TrackingId);
+// const TrackingId = 'UA-249995739-2';
+// ReactGA.initialize(TrackingId);
 
 
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  // useEffect(() => {
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
 
   return (
     <>
